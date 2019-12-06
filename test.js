be836252aac2f716e4dbb139ef4d63d3a558a804
monicalopezgris <monicalopezgris@gmail.com>
2019-12-06 19:07:48 +0100

    ADD example2

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
-var assert = require('assert');
-describe('Array', function () {
+const assert = require('assert');
+
+describe('Test group 1', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+  describe('sum is 4', () => {
+    it('should return 4', () => {
+      assert.equal(4, 2 + 2);
+    });
+    it('should return true', () => {
+      assert.equal(true, true);
     });
   });
 });
